3bb9fdb701e83153276d09c986624a2cdb5d820e
Aravind Naveen <aravindnaveen@Aravinds-MacBook-Air.local>
2023-10-21 18:36:31 +0530

    Updated Readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Blind-75
-Python solutions to the Blind 75 interview coding questions, complete with detailed explanations. Your one-stop resource for interview preparation.
+# Blind-75-Python-Solutions
+
+This repository contains my Python solutions to the "Blind 75" coding questions, a curated list of top interview questions often asked by tech companies. Whether you're preparing for technical interviews or looking to improve your algorithm and data structure skills, this collection of solutions provides a comprehensive resource to help you succeed. Each solution is accompanied by detailed explanations and comments, making it a valuable learning tool for developers of all levels.
+
+**Features:**
+- Python solutions to the Blind 75 coding questions.
+- Comprehensive explanations and comments for each solution.
+- Organized and categorized solutions for easy navigation.
+- A valuable resource for interview preparation and algorithm practice.
+
+Feel free to customize this description as needed, and best of luck with your repository!
--- Sliding Window/longest_substring_without_repeating_characters.py
+++ Sliding Window/longest_substring_without_repeating_characters.py
@@ -22,18 +22,3 @@ def test_longest_substring_without_repeating_character():
 if __name__ == "__main__":
     test_longest_substring_without_repeating_character()
     print("All test cases passed.")
-
-"""
-This repository contains my This repository contains my Python solutions to the "Blind 75" coding questions, a curated list of top interview questions often asked by tech companies. Whether you're preparing for technical interviews or looking to improve your algorithm and data structure skills, this collection of solutions provides a comprehensive resource to help you succeed. Each solution is accompanied by detailed explanations and comments, making it a valuable learning tool for developers of all levels.
-
-Features:
-
-Python solutions to the Blind 75 coding questions.
-Comprehensive explanations and comments for each solution.
-Organized and categorized solutions for easy navigation.
-A valuable resource for interview preparation and algorithm practice.
-Feel free to customize this description as needed, and best of luck with your repository!
-
-
-
-"""
